5c37e4c4725ec57288a7c481d855ab6ff3f69a2e
Apeksha M Prabhu <48539035+Apekshamarutip1@users.noreply.github.com>
2021-08-21 01:43:53 +0530

    Create sort123.java

---
+++ sort123.java
@@ -0,0 +1,48 @@
+/*Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+
+Example 1:
+
+Input: 
+N = 5
+arr[]= {0 2 1 2 0}
+Output:
+0 0 1 2 2
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.*/
+
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        // code here 
+        int lo = 0;
+        int hi = n - 1;
+        int mid = 0, temp = 0;
+        while (mid <= hi) {
+            switch (a[mid]) {
+            case 0: {
+                temp = a[lo];
+                a[lo] = a[mid];
+                a[mid] = temp;
+                lo++;
+                mid++;
+                break;
+            }
+            case 1:
+                mid++;
+                break;
+            case 2: {
+                temp = a[mid];
+                a[mid] = a[hi];
+                a[hi] = temp;
+                hi--;
+                break;
+            }
+            }
+        }
+        
+    }
+}
